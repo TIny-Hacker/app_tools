460f8be88837edd3f6e4afdf2ed1e072984c22c4
TIny_Hacker <thetiniesthacker@gmail.com>
2023-12-19 20:34:36 -0500

    Added an icon and some other small changes
    Co-Authored-By: RoccoLox Programs <roccoloxprograms2018@gmail.com>

--- make_installer.py
+++ make_installer.py
@@ -15,7 +15,7 @@ app_data += len(app_data).to_bytes(3, 'little')
 installer_src_path = os.path.dirname(os.path.realpath(__file__)) + "/installer.bin"
 installer_dest_path = f'{outdir}/{inst_name}.8xp'
 
-os.system(f'convbin -i "{installer_src_path}" -o "{installer_dest_path}" -j bin -k 8xp -n {inst_name}')
+os.system(f'convbin -i "{installer_src_path}" -o "{installer_dest_path}" -j bin -k 8xp -r -n {inst_name}')
 
 segments = [app_data[x:x+appvar_size] for x in range(0, len(app_data), appvar_size)]
 for (i, segment) in enumerate(segments):
